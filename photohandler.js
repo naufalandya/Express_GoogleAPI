dda4b96639289b99418b5e39f03a939745ace57f
Naufal Andya Faiz <135545118+naufalandya@users.noreply.github.com>
2023-12-16 23:15:15 +0700

    Update photohandler.js

--- photohandler.js
+++ photohandler.js
@@ -20,6 +20,8 @@ async function fetchImagesFromFolder(folderId) {
   return response.data.files;
 }
 
+//area 1
+
 app.get('/fetch-zoo1-area1-bella', async (req, res) => {
     try {
       const folderId = '1aY5lai4yqfzlD8i92RsMVfwbz19jwpSK';
@@ -34,7 +36,7 @@ app.get('/fetch-zoo1-area1-bella', async (req, res) => {
 
   app.get('/fetch-zoo1-area1-luna', async (req, res) => {
     try {
-      const folderId = '1ErvaAbguVp0LpGG28sc5ANwpNGoPIOQg';
+      const folderId = '1OnfMmpzzI60yr1YUUGydO4DGaRPxGu9e';
       const images = await fetchImagesFromFolder(folderId);
       const imageIds = images.map(image => image.id);
       res.json({ imageIds });
@@ -46,7 +48,7 @@ app.get('/fetch-zoo1-area1-bella', async (req, res) => {
 
   app.get('/fetch-zoo1-area2-fluffy', async (req, res) => {
     try {
-      const folderId = '1ErvaAbguVp0LpGG28sc5ANwpNGoPIOQg';
+      const folderId = '18sPhfJGvSE5kPVB9Dy2ZR-JKi8X_H6bu';
       const images = await fetchImagesFromFolder(folderId);
       const imageIds = images.map(image => image.id);
       res.json({ imageIds });
@@ -58,7 +60,19 @@ app.get('/fetch-zoo1-area1-bella', async (req, res) => {
   
   app.get('/fetch-zoo1-area2-oreo', async (req, res) => {
     try {
-      const folderId = '1ErvaAbguVp0LpGG28sc5ANwpNGoPIOQg'; //ini masih bisa kepake kayanya kalo mau nyoba,  
+      const folderId = '1Y9K46yOu1DVU-kjWVt8yfV2p7ifL7ToF';
+      const images = await fetchImagesFromFolder(folderId);
+      const imageIds = images.map(image => image.id);
+      res.json({ imageIds });
+    } catch (error) {
+      console.error('Error fetching image IDs:', error);
+      res.status(500).json({ error: 'An error occurred while fetching image IDs' });
+    }
+  });
+
+    app.get('/fetch-all', async (req, res) => {
+    try {
+      const folderId = '1TzWdjJadG34ChRlk3rrAsn4pMIiT6mzR';
       const images = await fetchImagesFromFolder(folderId);
       const imageIds = images.map(image => image.id);
       res.json({ imageIds });
